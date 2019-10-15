ee06570183b5e0e3e20e3a4d2e883f5c59955f0e
Sam Schmidt <sschmidt@physics.ucdavis.edu>
2019-10-15 13:36:59 -0700

    update testscript with new pythonpaths

--- test/testscript.sh
+++ test/testscript.sh
@@ -3,8 +3,8 @@
 #SBATCH --time=00:29:59
 #SBATCH --nodes=1
 #SBATCH --constraint=haswell
-#SBATCH --error="testfzboost.err"
-#SBATCH --output="testfzboost.out"
+#SBATCH --error="testfzboost23newpath.err"
+#SBATCH --output="testfzboost23newpath.out"
 
 module load python/3.7-anaconda-2019.07
 module swap PrgEnv-intel PrgEnv-gnu
@@ -17,7 +17,7 @@ module load gsl/2.5
 
 export CECI_SETUP="/global/projecta/projectdirs/lsst/groups/PZ/FlexZBoost/FlexZPipe/setup-flexz-cori-update"
 export HDF5_USE_FILE_LOCKING=FALSE
-export PYTHONPATH=$PYTHONPATH:/global/projecta/projectdirs/lsst/groups/PZ/FlexZBoost/install/lib/python3.7/site-packages:/global/projecta/projectdirs/lsst/groups/PZ/FlexZBoost/FlexZPipe:/global/homes/s/schmidt9/DESC/software/parsl/oldparsl/parsl-0.5.2/lib/lib/python3.7/site-packages:/global/homes/s/schmidt9/DESC/software/ceci/lib/lib/python3.7/site-packages:/global/homes/s/schmidt9/DESC/software/descformats/lib/python3.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/global/projecta/projectdirs/lsst/groups/PZ/FlexZBoost/install/lib/python3.7/site-packages:/global/projecta/projectdirs/lsst/groups/PZ/FlexZBoost/FlexZPipe:/global/projecta/projectdirs/lsst/groups/PZ/Packages/descformats/lib/python3.7/site-packages:/global/projecta/projectdirs/lsst/groups/PZ/Packages/ceci/lib/python3.7/site-packages:/global/projecta/projectdirs/lsst/groups/PZ/Packages/parsl0.5.2/lib/python3.7/site-packages
 
 
-srun -n 4 python3 -m flexzpipe FlexZPipe --photometry_catalog=./z_0_3_healpix_10326_magwerrSNtrim.hdf5 --config=./config.yml --photoz_pdfs=./outputs/z_0_3_testfile_pdfs.hdf5 --mpi
+srun -n 8 python3 -m flexzpipe FlexZPipe --photometry_catalog=./z_2_3.step_all.healpix_10326_magwerrSNtrim.hdf5 --config=./config.yml --photoz_pdfs=./outputs/z_2_3_testfile_pdfs_newpath.hdf5 --mpi
